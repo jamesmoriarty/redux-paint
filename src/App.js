bc074e05944960f780450b9c6b4cdc5b2a9f09d8
James Moriarty <jamespaulmoriarty@gmail.com>
2020-08-13 22:59:29 +1000

    feature: mvp with redux.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,63 @@
 import React from 'react';
 import './App.css';
 import { useRef, useEffect } from 'react';
+import { configureStore } from '@reduxjs/toolkit'
 
 function App() {
   let refCanvas = useRef(null),
     refCanvasContainer = useRef(null);
 
+  function mapMouseEvent(type, event) {
+    return {
+      type: type,
+      payload: {
+        x: event.pageX - refCanvas.current.offsetLeft,
+        y: event.pageY - refCanvas.current.offsetTop
+      }
+    }
+  };
+
+  function mouseReducer(state = { x: 0, y: 0 }, action) {
+    console.log(action)
+
+    if (refCanvas.current === null) return state
+
+    const ctx = refCanvas.current.getContext('2d')
+
+    switch (action.type) {
+      case "mousedown":
+        ctx.beginPath();
+        ctx.moveTo(action.payload.x, action.payload.y);
+        return { ...state, x: action.payload.x, y: action.payload.y };
+      case "mousemove":
+        ctx.lineTo(action.payload.x, action.payload.y);
+        ctx.stroke();
+        return { ...state, x: action.payload.x, y: action.payload.y };
+      default:
+        return state
+    }
+  }
+
+  const store = configureStore({ reducer: mouseReducer })
+
   useEffect(() => {
     const nodeCanvas = refCanvas.current,
       nodeCanvasContainer = refCanvasContainer.current,
-      stylesCanvasContainer = getComputedStyle(nodeCanvasContainer),
-      ctx = nodeCanvas.getContext('2d'),
-      onPaint = function () {
-        ctx.lineTo(mouse.x, mouse.y);
-        ctx.stroke();
-      };
+      stylesCanvasContainer = getComputedStyle(nodeCanvasContainer);
 
     nodeCanvas.width = parseInt(stylesCanvasContainer.getPropertyValue('width'));
     nodeCanvas.height = parseInt(stylesCanvasContainer.getPropertyValue('height'));
 
-    let mouse = { x: 0, y: 0 };
+    const onMouseMove = event => store.dispatch(mapMouseEvent("mousemove", event))
 
-    nodeCanvas.addEventListener('mousemove', function (e) {
-      mouse.x = e.pageX - this.offsetLeft;
-      mouse.y = e.pageY - this.offsetTop;
+    nodeCanvas.addEventListener('mousedown', (event) => {
+      store.dispatch(mapMouseEvent("mousedown", event))
+
+      nodeCanvas.addEventListener('mousemove', onMouseMove, false);
     }, false);
 
-    ctx.lineWidth = 3;
-    ctx.lineJoin = 'round';
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = '#00CC99';
-
-    nodeCanvas.addEventListener('mousedown', function (e) {
-      ctx.beginPath();
-      ctx.moveTo(mouse.x, mouse.y);
-
-      nodeCanvas.addEventListener('mousemove', onPaint, false);
-    }, false);
-
-    nodeCanvas.addEventListener('mouseup', function () {
-      nodeCanvas.removeEventListener('mousemove', onPaint, false);
+    nodeCanvas.addEventListener('mouseup', () => {
+      nodeCanvas.removeEventListener('mousemove', onMouseMove, false);
     }, false);
   })
 
