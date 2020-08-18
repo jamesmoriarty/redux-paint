8f10441937c7b3f204c2443d723cf283d27459a6
James Moriarty <jamespaulmoriarty@gmail.com>
2020-08-18 22:47:59 +1000

    Updates

---
+++ precache-manifest.7fcd5acb863c2608db6279e0c0c6a707.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4943493168fdf6a325852b6a0c575f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dc5819e32bb200ccd22",
+    "url": "/static/js/2.6415b139.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6415b139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c87d025a7f986f19f70c",
+    "url": "/static/js/main.8515ea6a.chunk.js"
+  },
+  {
+    "revision": "a25c580e802aa3083daa",
+    "url": "/static/js/runtime-main.99458a2c.js"
+  }
+]);
